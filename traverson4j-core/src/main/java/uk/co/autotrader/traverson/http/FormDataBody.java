c3c318f1d5590588a73dceb4677bda85f6cc036b
Shaun Storey <shaun@spssoftware.co.uk>
2019-05-23 14:13:38 +0100

    Implement FormDataBody (#4)
    * Implement FormDataBody
    
    * Implement FormDataBody
    
    * Implement FormDataBody

---
+++ traverson4j-core/src/main/java/uk/co/autotrader/traverson/http/FormDataBody.java
@@ -0,0 +1,78 @@
+package uk.co.autotrader.traverson.http;
+
+import java.nio.charset.Charset;
+import java.util.Arrays;
+
+/**
+ * FormData body
+ */
+public class FormDataBody implements Body<FormDataBody.NameValuePair[]> {
+    private final NameValuePair[] nameValuePairs;
+    private final Charset charset;
+
+    /**
+     * Constructs a FormDataBody
+     * @param charset see {@link Charset}
+     * @param nameValuePairs see {@link NameValuePair}
+     */
+    public FormDataBody(Charset charset, NameValuePair... nameValuePairs) {
+        this.nameValuePairs = Arrays.copyOf(nameValuePairs, nameValuePairs.length);
+        this.charset = charset;
+    }
+
+    /**
+     * @return the nameValuePairs that make up the request
+     */
+    @Override
+    public NameValuePair[] getContent() {
+        return Arrays.copyOf(nameValuePairs, nameValuePairs.length);
+    }
+
+    /**
+     * @return application/x-www-form-urlencoded
+     */
+    @Override
+    public String getContentType() {
+        return "application/x-www-form-urlencoded";
+    }
+
+    /**
+     * @return the charset for the request
+     */
+    public Charset getCharset() {
+        return charset;
+    }
+
+    /**
+     * A holder for a name value pair of the encapsulating FormData request
+     */
+    public static class NameValuePair {
+        private final String name;
+        private final String value;
+        /**
+         * Constructs a NameValuePair
+         * @param name see {@link NameValuePair#getName()}
+         * @param value see {@link NameValuePair#getValue()}
+         */
+        public NameValuePair(String name, String value) {
+            this.name = name;
+            this.value = value;
+        }
+
+        /**
+         * This is good practice to use and some API's may look for specific form data elements by name
+         * @return name of this form data field
+         */
+        public String getName() {
+            return name;
+        }
+
+        /**
+         * @return the value of this form data field
+         */
+        public String getValue() {
+            return value;
+        }
+    }
+
+}
---
+++ traverson4j-core/src/test/java/uk/co/autotrader/traverson/http/FormBodyTest.java
@@ -0,0 +1,24 @@
+package uk.co.autotrader.traverson.http;
+
+import com.google.common.base.Charsets;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(MockitoJUnitRunner.class)
+public class FormBodyTest {
+
+    @Test
+    public void init_GivenValues_SetsProperties() {
+
+        FormDataBody body = new FormDataBody(Charsets.UTF_8, new FormDataBody.NameValuePair("name", "value"));
+
+        assertThat(body.getContent()).hasSize(1);
+        assertThat(body.getContent()[0].getName()).isEqualTo("name");
+        assertThat(body.getContent()[0].getValue()).isEqualTo("value");
+        assertThat(body.getContentType()).isEqualTo("application/x-www-form-urlencoded");
+        assertThat(body.getCharset()).isEqualTo(Charsets.UTF_8);
+    }
+}
