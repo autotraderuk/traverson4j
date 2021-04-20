9ed94f0093afa06c7f9d5357d92bfeb341c96d6e
Michael.Rocke <michael.rocke@autotrader.co.uk>
2021-04-20 14:27:13 +0100

    Gradle upgrade to 7.0 Removing Bintray from this for now as publishing to jcenter no longer works Removed findbugs as that's not longer supported, replaced with spotbugs

--- traverson4j-hc5/src/test/java/uk/co/autotrader/traverson/http/IntegrationTest.java
+++ traverson4j-hc5/src/test/java/uk/co/autotrader/traverson/http/IntegrationTest.java
@@ -1,10 +1,11 @@
 package uk.co.autotrader.traverson.http;
 
 import com.alibaba.fastjson.JSONObject;
+import com.github.tomakehurst.wiremock.WireMockServer;
 import com.github.tomakehurst.wiremock.client.WireMock;
-import com.github.tomakehurst.wiremock.junit.WireMockRule;
-import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
-import org.junit.Rule;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import uk.co.autotrader.traverson.Traverson;
 
@@ -15,27 +16,42 @@ import static com.github.tomakehurst.wiremock.stubbing.Scenario.STARTED;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class IntegrationTest {
-    @Rule
-    @SuppressFBWarnings
-    public WireMockRule wireMockRule = new WireMockRule(8089);
+    private static WireMockServer wireMockServer;
 
     private final Traverson traverson = new Traverson(new ApacheHttpTraversonClientAdapter());
 
+    @BeforeClass
+    public static void beforeClass() throws Exception {
+        wireMockServer = new WireMockServer(8089);
+        wireMockServer.start();
+    }
+
+    @AfterClass
+    public static void afterClass() throws Exception {
+        wireMockServer.stop();
+        wireMockServer.resetAll();
+    }
+
+    @Before
+    public void setUp() throws Exception {
+        wireMockServer.resetAll();
+    }
+
     @Test
     public void requestBody_SimpleTextBodyIsSerializedAndPostedCorrectly() {
-        stubFor(patch(urlEqualTo("/records/1"))
+        wireMockServer.stubFor(patch(urlEqualTo("/records/1"))
                 .willReturn(WireMock.status(202)));
         Response<JSONObject> response = traverson.from("http://localhost:8089/records/1")
                 .patch(new TextBody("{\"key\":123}", "application/json", StandardCharsets.UTF_8));
 
-        verify(1, patchRequestedFor(urlEqualTo("/records/1")).withRequestBody(equalToJson("{\"key\":123}")));
+        wireMockServer.verify(1, patchRequestedFor(urlEqualTo("/records/1")).withRequestBody(equalToJson("{\"key\":123}")));
         assertThat(response.getStatusCode()).isEqualTo(202);
     }
 
     @Test
     public void requestBody_MultipartBodyIsSerializedAndPostedCorrectly() {
         byte[] data = new byte[]{0x00, 0x01, 0x02};
-        stubFor(post("/records")
+        wireMockServer.stubFor(post("/records")
                 .withMultipartRequestBody(aMultipart()
                         .withName("my-body-part")
                         .withHeader("Content-Type", equalTo("application/octet-stream"))
@@ -47,18 +63,18 @@ public class IntegrationTest {
         Response<JSONObject> response = traverson.from("http://localhost:8089/records")
                 .post(multipartBody);
 
-        verify(1, postRequestedFor(urlEqualTo("/records")));
+        wireMockServer.verify(1, postRequestedFor(urlEqualTo("/records")));
         assertThat(response.getStatusCode()).isEqualTo(202);
     }
 
     @Test
     public void basicAuthentication_ReactsToUnauthorizedStatusAndAuthenticateHeader() {
-        stubFor(get("/restricted-area")
+        wireMockServer.stubFor(get("/restricted-area")
                 .inScenario("Restricted access").whenScenarioStateIs(STARTED)
                 .willSetStateTo("First request made")
                 .willReturn(unauthorized().withHeader("WWW-Authenticate", "Basic realm=\"User Visible Realm\"")));
 
-        stubFor(get("/restricted-area")
+        wireMockServer.stubFor(get("/restricted-area")
                 .inScenario("Restricted access").whenScenarioStateIs("First request made")
                 .withBasicAuth("MyUsername", "MyPassword")
                 .willReturn(ok()));
@@ -68,12 +84,12 @@ public class IntegrationTest {
                                             .get(String.class);
 
         assertThat(response.getStatusCode()).isEqualTo(200);
-        verify(2, getRequestedFor(urlEqualTo("/restricted-area")));
+        wireMockServer.verify(2, getRequestedFor(urlEqualTo("/restricted-area")));
     }
 
     @Test
     public void basicAuthentication_GivenPreemptiveAuthenticationSetToTrue_SendsUsernameAndPasswordWithoutNeedingAnUnauthorizedResponse() {
-        stubFor(get("/restricted-area")
+        wireMockServer.stubFor(get("/restricted-area")
                 .withBasicAuth("MyUsername", "MyPassword")
                 .willReturn(ok()));
 
@@ -82,6 +98,6 @@ public class IntegrationTest {
                                             .get(String.class);
 
         assertThat(response.getStatusCode()).isEqualTo(200);
-        verify(1, getRequestedFor(urlEqualTo("/restricted-area")));
+        wireMockServer.verify(1, getRequestedFor(urlEqualTo("/restricted-area")));
     }
 }
