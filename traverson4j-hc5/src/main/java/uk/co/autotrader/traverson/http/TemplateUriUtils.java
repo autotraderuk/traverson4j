86a7c2a9c0be4bc9378af990e3a92d6e6ecae1aa
daniel.brown <daniel.brown@autotrader.co.uk>
2025-03-10 18:15:46 +0000

    added filtering for template params with empty values

--- traverson4j-hc5/src/main/java/uk/co/autotrader/traverson/http/TemplateUriUtils.java
+++ traverson4j-hc5/src/main/java/uk/co/autotrader/traverson/http/TemplateUriUtils.java
@@ -10,7 +10,9 @@ public class TemplateUriUtils {
     String expandTemplateUri(String templateUri, Map<String, List<String>> templateParams) {
         UriTemplate uriTemplate = UriTemplate.fromTemplate(templateUri);
         for (Map.Entry<String, List<String>> templateParam : templateParams.entrySet()) {
-            uriTemplate.set(templateParam.getKey(), templateParam.getValue());
+            if (!templateParam.getValue().isEmpty()) {
+                uriTemplate.set(templateParam.getKey(), templateParam.getValue());
+            }
         }
         return uriTemplate.expand();
     }
