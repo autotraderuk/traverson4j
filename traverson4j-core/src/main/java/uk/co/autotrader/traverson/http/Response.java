aa6376d5d2f529e5bf1e649215ed5a95d11f81c1
LaurenDarlaston <Lauren.Darlaston@autotrader.co.uk>
2025-04-22 12:35:37 +0100

    Correct a spelling mistake in the helper methods (#38)
    * Update response's handling of status codes
    
    * Update 3xx to list of failures
    
    * Add Tests
    
    * Correct spelling mistake
    
    * Correct spelling mistake

--- traverson4j-core/src/main/java/uk/co/autotrader/traverson/http/Response.java
+++ traverson4j-core/src/main/java/uk/co/autotrader/traverson/http/Response.java
@@ -46,7 +46,7 @@ public class Response<T> {
         return statusCodeFamily(2);
     }
 
-    public boolean isFaliure() {
+    public boolean isFailure() {
         return is3xx() || is4xx() || is5xx();
     }
 
